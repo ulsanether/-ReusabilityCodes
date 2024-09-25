0da1e456d9fe10ffdac4f8e5620a949c307fbb5d
ulsanether <hangmini12@naver.com>
2024-09-25 17:33:21 +0900

    넥션 lcd

---
+++ C/Nextion.c
@@ -0,0 +1,23 @@
+넥션 문자 보내기 
+
+
+Nextion_Send_String("t1.txt=\"16\"");
+
+void Netion_UART2_SendString(char *s)
+{
+    do
+    {TransmitChar_USART2(*s);
+        s++;
+    }while(*s != 0);
+}
+void Nextion_Send_String(char* string)
+{
+    Netion_UART2_SendString(string);
+    TransmitChar_USART2(0xFF);
+    TransmitChar_USART2(0xFF);
+    TransmitChar_USART2(0xFF);
+    delay_ms(1000);
+}
+
+
+
